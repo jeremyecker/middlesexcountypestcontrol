4ff8f6ea389ae2d6c3f16cde9013b1c11c46c54b
jeremyecker <jeremy@resteasypest.com>
2026-05-03 09:40:23 -0400

    fix: change trailingSlash: false to true (P3)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
+  trailingSlash: true,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
